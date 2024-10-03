ca68555c420b12095b4e40e6955ee89bec20f114
brendan <brendanchatt@gmail.com>
2024-10-03 19:05:02 -0400

    got the files and plaintext

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 
-import { TFile } from 'obsidian';
+import { link } from 'fs';
+import { TFile, getLinkpath } from 'obsidian';
 import { FileManager, MarkdownView, Plugin, } from 'obsidian';
 import { getAPI } from 'obsidian-dataview';
 
@@ -14,23 +15,33 @@ export default class MasterBoard extends Plugin {
 		
 		if(!view?.file) return;
 
-		let file = this.app.vault.getAbstractFileByPath(view.file.path);
+		let masterBoardFile = this.app.vault.getAbstractFileByPath(view.file.path);
 
-		if(!(file instanceof TFile)) return;
+		if(!(masterBoardFile instanceof TFile)) return;
 
-		let matter = await this.app.fileManager.processFrontMatter(file, (matter) => console.log(matter));
-		const cache = this.app.metadataCache.getFileCache(file);
-		let links = cache?.links;
-		console.log(cache?.frontmatterLinks);
+		//let matter = await this.app.fileManager.processFrontMatter(file, (matter) => console.log(matter));
+		const cache = this.app.metadataCache.getFileCache(masterBoardFile);
+		
+		cache?.frontmatterLinks?.forEach(async (fml) => {
+			let linkPath = getLinkpath(fml.link);
+			//console.log(linkPath);
 
-		// const path = this.app.metadataCache.getFirstLinkpathDest( getLinkpath('goin Down.mpr' ), 'Goin Down.mp3' )
-		// 	const url = this.app.vault.getResourcePath( path )
-		//console.log(links);
+			let sourceTFile = this.app.metadataCache.getFirstLinkpathDest(linkPath, masterBoardFile!.path);
+			if(!(sourceTFile instanceof TFile)) return;
+
+			let sourceBoardMetadata = this.app.metadataCache.getFileCache(sourceTFile!);
+			console.log(sourceBoardMetadata?.headings);
+
+			this.app.vault.cachedRead(sourceTFile).then((val)=> console.log(val.split('\n')));
+
+
+		});
+
 		// console.log(view?.editor.getValue());
 		// Grab tasks from linked pages
-		// const api = getAPI();
-		// console.log("pages:")
-		// console.log(api.pagesByLinkOrSomething()[0].file.tasks);
+	// 	const api = getAPI();
+	// 	console.log("pages:")
+	// console.log(api.pages()[0].file.tasks);
 
 	}
 }
